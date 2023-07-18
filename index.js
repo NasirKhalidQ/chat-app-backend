557da8f42b23b72750a7b457829dd651e328f2db
Nasir Khalid <nasir.khalid.new@gmail.com>
2023-07-18 23:20:55 +0500

    added user model and routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const userRoute = require("./Routes/userRoutes")
 
 const app = express()
 require("dotenv").config()
 app.use(express.json())
 app.use(cors())
+app.use("/api", userRoute)
 
 const port = 8000
 
