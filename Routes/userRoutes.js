ab85107214276299758a7fd351b20ce4bfd7ac01
Nasir Khalid <nasir.khalid.new@gmail.com>
2023-07-18 23:27:15 +0500

    added auth packages

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {registerUser} = require("../Controllers/userController")
 
 const router = express.Router()
-
+const jwt = require("jsonwebtoken")
 
 router.post("/register", registerUser)
 
