ed626ab6d42da7ddddb99f92938d58f484810ac7
Nasir Khalid <nasir.khalid.new@gmail.com>
2023-07-18 23:10:58 +0500

    setup mongoose

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
-
+require("dotenv").config()
 app.use(express.json())
 app.use(cors())
 
@@ -10,4 +11,10 @@ const port = 8000
 
 app.listen(port, (req, res)=>{
     console.log(`Server running on port ${port}`)
+})
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('Mongo connected')
+}).catch(err=>{
+    console.log(err.message, '//error')
 })
