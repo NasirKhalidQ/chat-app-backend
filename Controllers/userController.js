2bae7839bd2c1152a07b09de88f4267ec0e95596
Nasir Khalid <nasir.khalid.new@gmail.com>
2023-07-18 23:57:59 +0500

    added api for logging user in

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -38,4 +38,24 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await userModel.findOne({ email });
+
+    if (!user) return res.status(400).json("Invalid email or password...");
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) return res.status(400).json("Incorrect Password");
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user.id, name: user.name, email, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { registerUser, loginUser };
