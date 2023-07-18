8e46383a9f64a5f461bb89bd4bedc995834be5af
Nasir Khalid <nasir.khalid.new@gmail.com>
2023-07-18 23:49:43 +0500

    added register api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,10 +1,41 @@
-const userModel = require("../Models/userModel")
+const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
 
+const createToken = (_id) => {
+  const jwtKey = process.env.JWT_SECRET_KEY;
 
-const registerUser = (req, res) => {
-    res.send()
-}
+  return jwt.sign({ _id }, jwtKey, { expiresIn: "3d" });
+};
 
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    let user = await userModel.findOne({ email });
 
+    if (user) return res.status(400).json("User with email already exists");
 
-module.exports = {registerUser}
+    if (!name || !email || !password)
+      return res.status(400).json("All fields are required");
+
+    if (!validator.isEmail(email))
+      return res.status(400).json("Email is invalid");
+
+    user = new userModel({ name, email, password });
+
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ _id: user.id, name, email, token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { registerUser };
--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const {registerUser} = require("../Controllers/userController")
 
 const router = express.Router()
-const jwt = require("jsonwebtoken")
 
 router.post("/register", registerUser)
 
